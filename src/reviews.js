40ff44c5fb4960372fe1528708aeb8323dd3f9f2
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-06 14:39:53 -0700

    added reviews file

---
+++ src/reviews.js
@@ -0,0 +1,37 @@
+export const reviews = [
+    {
+        name: 'Catherine',
+        location: 'Orlando, Florida',
+        text: 'Rebecca was amazing from start to finish! She is very knowledgeable about Hawaii and her recommendations were spot on and not the typical touristy type. The location was perfect. She stayed under budget, which is always appreciated. I would 100% recommend her to anyone and will use her services again in the future.'
+    },
+    {
+        name: 'Dan',
+        location: 'St. Louis, MO',
+        text: 'Rebecca went out of her way to book our family vacation to Playa Del Carmen, Mexico. What she did was above and beyond our expectations and I cannot believe the deal she got us! To top it off she had a local connection in Cancun so we got an amazing private tour of the Mayan ruins. We will be going with Rebecca for all of our family trips now. Thank you Rebecca!'
+    },
+    {
+        name: 'Shauna',
+        location: 'Seattle, WA',
+        text: 'Doing a Mystery Trip can be a little unnerving but Rebecca made the experience unique and fun. She made sure I had plenty of unplanned days for me to do whatever I wanted, but she also planned a couple day trips according to my interests. She really cared and wanted me to have a good time and I felt comfortable working with her. I will definitely be using her again in the future for another mystery trip!'
+    },
+    {
+        name: 'Anais',
+        location: 'Orlando, FL',
+        text: 'Rebecca was amazing!!! She helped plan our honeymoon in Mexico. She was very patient with us although we switched the itinerary and locations around a few times before finalizing it. Rebecca also worked with our budget and didn’t pressure us to spend more money than we had planned to. We told her what was important to us which was budget, location, and comfortable for our honeymoon and we couldn’t have been happier with the selections suggested by her. She made the process of having to plan a honeymoon while also planning a wedding so much easier. Will definitely recommend her to anyone and will definitely use Rebecca again for future travels.'
+    },
+    {
+        name: 'Matt',
+        location: 'Rancho Mirage, California',
+        text: "We had the very fortunate opportunity to work with Rebecca in what ended up being an unfortunate and complex issue. Our friend's wedding was to be in Mexico during the COVID-19 pandemic and unfortunately needed to be cancelled. However, with the assistance of Rebecca we stayed completely informed and she expertly navigated all parties through the process. We were so impressed we will be using her in the future for our adventures! I recommend her with the highest compliments and without reservation. She is thorough, responsive and efficient. Truly the silver lining in an otherwise heartbreaking situation."
+    },
+    {
+        name: 'Amanda',
+        location: 'Hanover, CT',
+        text: "Rebecca was extremely easy to access. Always willing to answer questions or make suggestions, even at the last minute. Flexible, friendly and knowledgeable! I would highly recommend working with Rebecca and her team. We went to Maui and Kauai and I am SO glad we had her in our corner to lead us in the right direction. Perfect vacation, exactly what we asked for."
+    },
+    {
+        name: 'Joel',
+        location: 'Gresham, OR',
+        text: "Rebecca is amazing to work with. It was such a smooth and custom-made vacation to Maui that we will only use her from now on. She is incredibly responsive down to the minute we felt totally relaxed and didn’t worry about a thing!"
+    }
+];
